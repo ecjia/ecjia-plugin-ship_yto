940befc086208c63cc83dc55596ec4f97b0d7489
王正东 <huangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comxhuangyuyuan@ecmoban.comrhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comyhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comghuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2017-11-28 11:05:09 +0800

    更换配送方式插件的继承类ShippingAbstract

--- ship_yto.class.php
+++ ship_yto.class.php
@@ -51,9 +51,9 @@
 
 defined('IN_ECJIA') or exit('No permission resources.');
 
-RC_Loader::load_app_class('shipping_abstract', 'shipping', false);
+use Ecjia\App\Shipping\ShippingAbstract;
 
-class ship_yto extends shipping_abstract
+class ship_yto extends ShippingAbstract
 {
     /**
      * 获取插件代号
@@ -94,17 +94,6 @@ class ship_yto extends shipping_abstract
     public function printBcakgroundImage()
     {
         return $this->config['print_bg'];
-    }
-    
-    /**
-     * 获取插件配置信息
-     */
-    public function configure_config() {
-        $config = include(RC_Plugin::plugin_dir_path(__FILE__) . 'config.php');
-        if (is_array($config)) {
-            return $config;
-        }
-        return array();
     }
 
     /**
